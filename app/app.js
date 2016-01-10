77791ae675de8b315182cf0b81f51abf26f8b7d6
Med Belamachi <med.belamachi@gmail.com>
2016-01-10 23:55:03 +0000

    adding angular ui router

--- app/app.js
+++ app/app.js
@@ -1,12 +1,43 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+angular.module('app', [
+    'ui.router',
+    'app.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/view1');
+
+    $stateProvider
+        .state('view1', {
+            url: "/view1",
+            views: {
+                'header': {
+                    templateUrl: "templates/partials/header/header.html"
+                },
+                /*'navPanel': {
+                 templateUrl: "templates/partials/header/navigationPanel.html"
+                 },*/
+                'content': {
+                    templateUrl: "view1/view1.html",
+                    controller: 'View1Ctrl as view1Ctrl'
+                }
+            }
+
+        }).state('view2', {
+        url: "/view2",
+        views: {
+            'header': {
+                templateUrl: "templates/partials/header/header.html"
+            },
+            /*'navPanel': {
+             templateUrl: "templates/partials/header/navigationPanel.html"
+             },*/
+            'content': {
+                templateUrl: "view2/view2.html",
+                controller: 'View2Ctrl as view2Ctrl'
+            }
+        }
+    });
 }]);
